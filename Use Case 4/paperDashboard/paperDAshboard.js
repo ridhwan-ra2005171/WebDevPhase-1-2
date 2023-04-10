0269a0829f1b63a3186bc65857fd7ab6050ae657
Mohamad-ma2005872 <mohamadallahampersonal@gmail.com>
2023-04-10 23:58:11 +0300

    use case 4 updates

--- Use Case 4/paperDashboard/paperDAshboard.js
+++ Use Case 4/paperDashboard/paperDAshboard.js
@@ -1,37 +1,91 @@
-const cardList = document.querySelector("#card-list")
-// console.log(cardList);
-async function displayPapers() {
+// Commonly used URLS 
+let usersUrl = "../../users.json";
 
+// assuming we have the gloabl variable USER_ID
+// we set it to 7 for testing sake only
+const USER_ID = 7;
+
+// Elements from html
+const cardList = document.querySelector("#card-list");
+const body = document.querySelector("body-content")
+
+
+// console.log(cardList);
+async function displayPapers(papersUrl) {
   // window.location = "../reviewPaper/reviewPaper.html";
-  
-    if (!localStorage.papersloc) {
-        // if the recipes dont exist in the local storage, create one and set
-        // i declared recipes as global variable in line 7
-        papersloc  = await (await fetch(papersUrl)).json();
-        localStorage.setItem("papersloc", JSON.stringify(papersloc));
-        papersloc = JSON.parse(localStorage.papersloc);
-        cardList.innerHTML = papersloc
-          .map((p) => cardTemplate(p))
-          .join("");
-      } else {
-        // recipe array exists in the local storage, retrieve it
-        papersloc = JSON.parse(localStorage.papersloc);
-        // paperTitle.innerHTML 
-        cardList.innerHTML = papersloc
-          .map((p) => recipeToHTML(p))
-          .join("");
-      }
+  users  = await (await fetch(usersUrl)).json();
+  localStorage.setItem('usersloc',JSON.stringify(users));
+  // console.log(localStorage.usersloc);
+  // check if papers belonging to this current reviewer are in local storage
+  // if (!localStorage.papersloc || (parseInt(localStorage.papersloc[0].pid) !== parseInt(USER_ID))) {
+  // if the papers dont exist in the local storage,
+  papers = await (await fetch(papersUrl)).json();
+  // get only papers that belong to the current reviewer using the global USER_ID
+  console.log(papers);
+  assignedPapers = papers.filter((paper) =>
+    getPaperOfReviewer(paper, USER_ID)
+  );
+  console.log(assignedPapers);
+
+  localStorage.setItem("assignedPapers", JSON.stringify(assignedPapers));
+  assignedPapers = JSON.parse(localStorage.assignedPapers);
+  cardList.innerHTML = assignedPapers
+    .map((paper) => cardTemplate(paper))
+    .join("");
+
+  // } else {
+  // recipe array exists in the local storage, retrieve it
+  // papersloc = JSON.parse(localStorage.papersloc);
+  // // paperTitle.innerHTML
+  // cardList.innerHTML = papersloc
+  //   .map((p) => recipeToHTML(p))
+  //   .join("");
+  // }
+}
+
+function getPaperOfReviewer(paper, reviewerId) {
+  if (paper.reviewers.find((id) => id === reviewerId))
+    // console.log(paper.reviewers.find(id => id===7 ));
+    return paper;
+}
+
+async function loadPage(pageUrl,paperId) {
+  localStorage.setItem('paperAtm',String(paperId))
+  console.log(localStorage.paperAtm);
+  window.location.href = pageUrl;
+  // const page = await fetch(pageUrl);
+  // body.innerHTML = page.text();
+
+
+  // const mainContent = document.querySelector("#main-content");
+  // const pageHTMLContent = await page.text();
+  // mainContent.innerHTML = pageHTMLContent;
 }
 
 function cardTemplate(paper) {
-  return `<div class="card">
-          <a href="#" onclick="loadPage('../reviewPaper/reviewPaper.html',this.id)">
-              <h4 id="title-cont">${paper.title}</h4>
-          </a>
-          <p id="abstract-container" class="abstract-container">
-            ${paper.abstract}
-          </p>
-          <p>${paper.filter()}</p>
-          <p>dd-MM-yyyy . HH:MM</p>
-        </div>`;
+  // return `<div class="card">
+  //         <a href="#" onclick="loadPage('../reviewPaper/reviewPaper.html',this.id)">
+  //             <h4 id="title-cont">${paper.title}</h4>
+  //         </a>
+  //         <p id="abstract-container" class="abstract-container">
+  //           ${paper.abstract}
+  //         </p>
+  //         <p>${paper.filter()}</p>
+  //         <p>dd-MM-yyyy . HH:MM</p>
+  //       </div>`;
+  return `
+  <div class="card">
+      <a href="#" onclick="loadPage('../reviewPaper/reviewPaper.html',${paper.pid})">
+          <h3 id="paper-title">${paper.title}</h3>
+      </a>
+      <!-- <p class="abstract-container">Abstract Abstract Abstract Abstract
+          Abstract Abstract Abstract Abstract
+          Abstract Abstract Abstract Abstract
+          Abstract Abstract Abstract Abstract
+          Abstract Abstract Abstract Abstract
+      </p> -->
+      <p id="paper-authors">${paper.authors.filter(auth => {auth}).join(";")}</p>
+      <p id="paper-date">${paper.date}</p>
+      <!-- <i class="fa fa-"></i> -->
+  </div>`;
 }
--- Use Case 4/reviewPaper/reviewPaper.js
+++ Use Case 4/reviewPaper/reviewPaper.js
@@ -1,5 +1,5 @@
 // Imports
-const papersUrl = "../../papers.json"; 
+const papersUrl = "../../papers.json";
 const usersUrl = "../../users.json";
 
 let papersloc = null;
@@ -10,42 +10,41 @@ const paperCont = document.querySelector("#paper-cont");
 
 //load data into/from local storage
 async function getData() {
-    papers  = await (await fetch(papersUrl)).json();
-    users  = await (await fetch(usersUrl)).json();
-    // let test = users.filter((index, user) => user.id === (papers[index].authors[index]));
-    test = users.find((u) => {
-        if (papers[0].authors.includes(u.id)) {
-            // return `${u.last_name}, ${u.first_name}`;   
-            return 4;
-        }})
-        
-    console.log(test);
-    // if (!localStorage.papersloc) {
-        // if the recipes dont exist in the local storage, create one and set
-        // i declared recipes as global variable in line 7
-        // papersloc  = await (await fetch(papersUrl)).json();
-        // localStorage.setItem("papersloc", JSON.stringify(papersloc));
-        // papersloc = JSON.parse(localStorage.papersloc);
-        // recipesContainer.innerHTML = papersloc
-        //   .map((p) => cardTemplate(p))
-        //   .join("");
-    //   } else {
-        // recipe array exists in the local storage, retrieve it
-        // papersloc = JSON.parse(localStorage.papersloc);
-        // paperTitle.innerHTML 
-        // paperCont.innerHTML = papersloc
-        //   .map((recipe) => recipeToHTML(recipe))
-        //   .join("");
-    //   }
+  papers = await (await fetch(papersUrl)).json();
+  users = await (await fetch(usersUrl)).json();
+  // let test = users.filter((index, user) => user.id === (papers[index].authors[index]));
+  test = users.find((u) => {
+    if (papers[0].authors.includes(u.id)) {
+      // return `${u.last_name}, ${u.first_name}`;
+      return 4;
+    }
+  });
+
+  console.log(test);
+  // if (!localStorage.papersloc) {
+  // if the recipes dont exist in the local storage, create one and set
+  // i declared recipes as global variable in line 7
+  // papersloc  = await (await fetch(papersUrl)).json();
+  // localStorage.setItem("papersloc", JSON.stringify(papersloc));
+  // papersloc = JSON.parse(localStorage.papersloc);
+  // recipesContainer.innerHTML = papersloc
+  //   .map((p) => cardTemplate(p))
+  //   .join("");
+  //   } else {
+  // recipe array exists in the local storage, retrieve it
+  // papersloc = JSON.parse(localStorage.papersloc);
+  // paperTitle.innerHTML
+  // paperCont.innerHTML = papersloc
+  //   .map((recipe) => recipeToHTML(recipe))
+  //   .join("");
+  //   }
 }
 
 // call load data
-getData()
+// getData()
 
 // const test = papers[0].authors;
-console.log(papers);
-
-
+// console.log(papers);
 
 // Paper Elements
 const paperTitle = document.querySelector("#title");
@@ -62,49 +61,95 @@ const backBtn = document.querySelector("#go-back");
 const cancelBtn = document.querySelector("#cancel");
 const submitBtn = document.querySelector("#submit");
 
-//load paper data into page
-async function getPapers(){
-    const papers = await getData(papersUrl);
-//  console.log(papers[1].title);
-// paperTitle.innerHTML = papers[0].title;
-
-}
-getPapers()
-
 // Event listeners for buttons
-backBtn.addEventListener('click',returnToPage)
-cancelBtn.addEventListener('click',cancelReview)
+backBtn.addEventListener("click", returnToPrevPage);
+cancelBtn.addEventListener("click", cancelReview);
+
+//load paper
+async function laodPaper() {
+  papers = await (await fetch(papersUrl)).json();
+  users = await (await fetch(usersUrl)).json();
+
+  const reviewPaper = papers.find(
+    (p) => p.pid === parseInt(localStorage.paperAtm)
+  );
+
+  const authorsObjects = users.filter((o1) => reviewPaper.authors.find((o2) => o1.id === o2));
+  let authorsNames = authorsObjects.map((author) => {
+    return `${author.first_name} ${author.last_name}`;
+  });
+  console.log(authorsNames.join("; "));
+
+  // console.log(reviewPaper);
+  paperTitle.innerHTML = reviewPaper.title;
+  paperAuthors.innerHTML = authorsNames.join("; ");
+//   console.log("Me");
+//   let result = users.filter((o1) => {
+    // reviewPaper.authors.find((o2) => o1.id === o2);
+    // return `${o1.first_name} ${o1.last_name}`;
+//   });
+
+  
+//   console.log(result);
+
+ 
+
+
+  // console.log(getAuthorName());
+}
+
+laodPaper();
+
+function getAuthorName(userId, authorId) {
+  let result = users.filter((o1) =>
+    reviewPaper.authors.find((o2) => o1.id === o2)
+  );
+  result = result.forEach((author) => {
+    return `${author.first_name} ${author.last_name}`;
+  });
+  console.log(result);
+}
+
+// getAuthorName()
+// //load paper data into page
+// async function getPapers(){
+//     const papers = await getData(papersUrl);
+// //  console.log(papers[1].title);
+// // paperTitle.innerHTML = papers[0].title;
+// }
+
+// getPapers()
 
 // The event listener functions
-async function returnToPage(e) {
-    e.preventDefault();
-    // swal("Are you sure you want to cancel?","","warning",{ buttons: ["Yes","No"],));
+// return to previous page
+async function returnToPrevPage(e) {
+  e.preventDefault();
+  // swal("Are you sure you want to cancel?","","warning",{ buttons: ["Yes","No"],));
 
-    let result = await swal({
-        title: "Your changes will not be saved!",
-        dangerMode: true,
-        icon:"error",
-        buttons: ["Cancel", "Proceed"],
-      });
+  let result = await swal({
+    title: "Your changes will not be saved!",
+    dangerMode: true,
+    icon: "error",
+    buttons: ["Cancel", "Proceed"],
+  });
 
-    // console.log(result);
-    if (result === true) {
-        location.href = "../paperDashboard/paperDashboard.html"
-    }
+  // console.log(result);
+  if (result === true) {
+    location.href = "../paperDashboard/paperDashboard.html";
+  }
 }
 
 async function cancelReview(e) {
-    e.preventDefault();
-    // alert("Are you sure you want to cancel?")
-    let result = await swal({
-        title: "Are you sure you want to cancel?",
-        icon:"warning",
-        buttons: ["No, stay", "Yes, cancel"],
-      });
-      
-    // console.log(result);
-    if (result === true) {
-        location.href = "../paperDashboard/paperDashboard.html"
-    } 
-}
+  e.preventDefault();
+  // alert("Are you sure you want to cancel?")
+  let result = await swal({
+    title: "Are you sure you want to cancel?",
+    icon: "warning",
+    buttons: ["No, stay", "Yes, cancel"],
+  });
 
+  // console.log(result);
+  if (result === true) {
+    location.href = "../paperDashboard/paperDashboard.html";
+  }
+}
