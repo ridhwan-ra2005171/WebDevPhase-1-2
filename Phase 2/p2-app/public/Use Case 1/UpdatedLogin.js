93dfba0fc25cb996d4b5aec8dc66cc4df1a47d6b
saoud-sa1908762 <112058834+saoud-sa1908762@users.noreply.github.com>
2023-06-02 14:14:03 +0300

    added updated js for login

---
+++ Phase 2/p2-app/public/Use Case 1/UpdatedLogin.js
@@ -0,0 +1,32 @@
+// login.js
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    if (user && user.password === password) {
+      if (user.role === "author") {
+        window.location.href = "submit.html";
+      } else if (user.role === "reviwer") {
+        window.location.href = "reviewPapers.html";
+      } else {
+        window.location.href = "conferenceSch.html";
+      }
+    }
+  } catch (error) {
+    console.error("User not found", error);
+  }
+});
