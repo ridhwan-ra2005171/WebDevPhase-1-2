40f16030547bb784f215630dbc44ff4348764f81
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2023-06-06 09:40:55 +0300

    pull

--- Phase 2/p2-app/app/api/locations/[id]/route.js
+++ Phase 2/p2-app/app/api/locations/[id]/route.js
@@ -4,6 +4,7 @@ import locationRepo from "../location-repo";
 export async function GET(request, { params }) {
     try {
         const { id } = params
+        //remove parseInt once it has been changed
         const location = await locationRepo.getLocation(id);
         return Response.json(location, { status: 200 });
     } catch (e) {
